c76d4a5c14c0dda8e8a86c8d2d507b438871d4b5
Bengt Månsson <bengt.mansson@gmail.com>
2024-12-04 21:05:27 +0100

    not working again

--- src/components/Details2.jsx
+++ src/components/Details2.jsx
@@ -8,6 +8,7 @@ import {
     LineElement,
     Title,
     Tooltip,
+    TimeScale,
     Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
@@ -21,6 +22,7 @@ ChartJS.register(
     PointElement,
     LineElement,
     Title,
+    TimeScale,
     Tooltip,
     Legend
 );
@@ -36,99 +38,112 @@ const age = (t) => {
 function Details2(props) {
     const [checkTemp, setCheckTemp] = useState(true);
     const [checkHum, setCheckHum] = useState(false);
-    if (props.data === undefined) return <div>no data</div>;
 
-    const handleChangeTemp = () => {
-        setCheckTemp(!checkTemp);
-    }
-    const handleChangeHum = () => {
-        setCheckHum(!checkHum);
-    }
+    console.log("props", props);
+    if (props.data === undefined) return <div>no data</div>; else {
+        if (props.data.readings === undefined) return <div>no data</div>; else {
+            if (props.data.readings[0].dt === undefined) return <div>no data</div>; else {
+                const minValue = props.data.readings[0].dt;
+                const maxValue = props.data.readings[props.data.readings.length].dt;
 
-    const options = {
-        responsive: true,
-        // maintainAspectRatio: false,
-        aspectRatio: 1,
-        interaction: {
-            mode: 'nearest',
-            intersect: true,
-        },
-        stacked: false,
-        plugins: {
-            title: {
-                display: true,
-                text: 'Sensor history',
-            },
-        },
-        scales: {
-            x: [{
-                type: 'time',
-                time: {
-                    unit: 'hour',
-                    tooltipFormat: 'DD H',
-                    displayFormats: {
-                        hour: 'DD H'
-                    }
+                const handleChangeTemp = () => {
+                    setCheckTemp(!checkTemp);
                 }
-            }],
-            y: {
-                type: 'linear',
-                display: checkTemp,
-                position: 'left',
-                beginAtZero: false,
-            },
-            y2: {
-                type: 'linear',
-                display: checkHum,
-                position: 'right',
-                beginAtZero: false,
-                grid: {
-                    display: false,
-                },
-            },
-        },
+                const handleChangeHum = () => {
+                    setCheckHum(!checkHum);
+                }
+
+                const options = {
+                    responsive: true,
+                    // maintainAspectRatio: false,
+                    aspectRatio: 1,
+                    interaction: {
+                        mode: 'nearest',
+                        intersect: true,
+                    },
+                    stacked: false,
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Sensor history',
+                        },
+                    },
+                    scales: {
+                        x: {
+                            type: 'time',
+                            time: {
+                                unit: 'hour',
+                                tooltipFormat: 'DD H',
+                                displayFormats: {
+                                    hour: 'DD H'
+                                },
+                                ticks: {
+                                    min: minValue,
+                                    max: maxValue,
+                                }
+                            }
+                        },
+                        y: {
+                            type: 'linear',
+                            display: checkTemp,
+                            position: 'left',
+                            beginAtZero: false,
+                        },
+                        y2: {
+                            type: 'linear',
+                            display: checkHum,
+                            position: 'right',
+                            beginAtZero: false,
+                            grid: {
+                                display: false,
+                            },
+                        },
+                    },
+                };
+                const readings = props.data.readings;
+                const labels = readings.map(e => { const d = new Date(e.dt); return d.getHours() + ":" + d.getMinutes() });
+                const data = {
+                    labels,
+                    datasets: [
+                        checkTemp && {
+                            label: 'Temperature',
+                            data: readings.map(e => { return e.t; }),
+                            pointRadius: 0,
+                            borderWidth: 1,
+                            borderColor: 'rgb(255, 99, 132)',
+                            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                            yAxisID: 'y',
+                        },
+                        checkHum && {
+                            label: 'Humidity',
+                            data: readings.map(e => { return e.h; }),
+                            pointRadius: 0,
+                            borderWidth: 1,
+                            borderColor: 'rgb(99, 255, 132)',
+                            backgroundColor: 'rgba(99, 255, 132, 0.5)',
+                            yAxisID: 'y2',
+                        },
+                    ],
+                };
+                return (
+                    <div>
+                        <p>
+                            Last server read {new Date(props.time).toLocaleString('sv-SE', { timeZone: 'CET' })}
+                            <br />
+                            {readings.length} samples between {new Date(readings[0].dt).toLocaleString('sv-SE', { timeZone: 'CET' })}
+                            <br />
+                            and {new Date(readings[readings.length - 1].dt).toLocaleString('sv-SE', { timeZone: 'CET' })}
+                            <br />Last sensor data is {age(new Date(readings[readings.length - 1].dt))} old
+                        </p>
+                        <Checkbox label="temp" value={checkTemp} onChange={handleChangeTemp} />
+                        <Checkbox label="hum" value={checkHum} onChange={handleChangeHum} />
+                        <div style={{ position: "relative", height: "700" }}>
+                            <Line data={data} options={options} />
+                        </div>
+                    </div>
+                )
+            };
+        };
     };
-    const readings = props.data.readings;
-    const labels = readings.map(e => { const d = new Date(e.dt); return d.getHours() + ":" + d.getMinutes() });
-    const data = {
-        labels,
-        datasets: [
-            checkTemp && {
-                label: 'Temperature',
-                data: readings.map(e => { return e.t; }),
-                pointRadius: 0,
-                borderWidth: 1,
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                yAxisID: 'y',
-            },
-            checkHum && {
-                label: 'Humidity',
-                data: readings.map(e => { return e.h; }),
-                pointRadius: 0,
-                borderWidth: 1,
-                borderColor: 'rgb(99, 255, 132)',
-                backgroundColor: 'rgba(99, 255, 132, 0.5)',
-                yAxisID: 'y2',
-            },
-        ],
-    };
-    return (
-        <div>
-            <p>
-                Last server read {new Date(props.time).toLocaleString('sv-SE', { timeZone: 'CET' })}
-                <br />
-                {readings.length} samples between {new Date(readings[0].dt).toLocaleString('sv-SE', { timeZone: 'CET' })}
-                <br />
-                and {new Date(readings[readings.length - 1].dt).toLocaleString('sv-SE', { timeZone: 'CET' })}
-                <br />Last sensor data is {age(new Date(readings[readings.length - 1].dt))} old
-            </p>
-            <Checkbox label="temp" value={checkTemp} onChange={handleChangeTemp} />
-            <Checkbox label="hum" value={checkHum} onChange={handleChangeHum} />
-            <div style={{ position: "relative", height: "700" }}>
-                <Line data={data} options={options} />
-            </div>
-        </div>
-    );
 };
-export default Details2
+export default Details2;
